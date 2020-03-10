d4e0f6adf0b8ec1cb7279dc064ce23f1e9986857
Bruce Ruffin <bruffin1221@gmail.com>
2020-03-10 05:44:28 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -79,8 +79,8 @@ end
 def can_update_using_update_method
   # Update movie title to "Wat, huh?"
   Movie.create(title: "Wat?")
-  theMovie = Movie.find_by(title: "Awesome Flick")
-  theMovie.title = "Even Awesomer Flick"
+  theMovie = Movie.find_by(title: "Wat")
+  theMovie.title = "Wat Duh"
   theMovie.save
   theMovie
 end
