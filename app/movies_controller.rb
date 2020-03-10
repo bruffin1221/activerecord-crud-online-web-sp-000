0f47f6aee4d1c256de75ecc01dd67d7a3a5fc60c
Bruce Ruffin <bruffin1221@gmail.com>
2020-03-10 05:36:37 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -79,7 +79,7 @@ end
 def can_update_using_update_method
   # Update movie title to "Wat, huh?"
   Movie.create(title: "Wat?")
-  movie = movie.find_by(title: "Wat")
+  movie = Movie.find_by(title: "Wat")
   movie.update(name: "Wat, huh?")
 end
 
